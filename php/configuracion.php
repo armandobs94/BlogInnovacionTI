f91c77c2936042d1df161dcb75b744378d6a3255
Armando <armandobs94@gmail.com>
2016-02-10 19:19:08 -0600

    Se agrego el menú al index
    Agregación del index al proyecto, corrección de directorios del
    proyecto.

--- php/configuracion.php
+++ php/configuracion.php
@@ -8,4 +8,10 @@
 	// define('USER', "root");
 	// define('PASSW',"");
 	// define('BD', "db_blog");
+
+	$saml_lib_path = '/var/simplesamlphp/lib/_autoload.php';  //La ruta donde se encuentra la librería principal de simplesamlphp
+    require_once($saml_lib_path);
+    $SP_URL = 'http://'.$_SERVER['SERVER_NAME'].":8085/demo";  // url de nuestro servidor.
+    $SP_ORIGEN= 'borrar';   // Fuente de autenticación definida en el authsources del SP
+    $saml = new SimpleSAML_Auth_Simple($SP_ORIGEN);   // Se crea la instancia del saml, pasando como parametro la fuente de autenticación.
  ?>
